a043a4fcefb137538e61e30f8bc92651ef212c3b
OmairHassan <ohassan@stu.naperville203.org>
2023-04-06 11:50:31 -0500

    Made it so that Schmiipy can't wall jump, but the part where you jump onto a building right next to the previous one doesn't let you jump

--- Game Design Group Project/Assets/AnimationController.cs
+++ Game Design Group Project/Assets/AnimationController.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     Animator animator;
-    //static float x = 0.0f;
-    //static float y = 0.0f;
     static float x = 0f;
     static float y = 0f;
+    static float temp = 1f;
     static int jumpCount = 0;
+    static int number = 0;
+    static bool falling = false;
     Vector2 position;
-    //public Vector3 myPos;
-    //public Transform myPlay;
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -23,15 +21,37 @@ public class AnimationController : MonoBehaviour
     void Update()
     {
         x = transform.position.x;
+        //temp = y;
         y = transform.position.y;
-        bool OnPlatform = Platform.getOnPlatform();
-        if (Input.GetKey(KeyCode.W) && OnPlatform == true || jumpCount > 0)
+        if(temp != y)
         {
-            if(jumpCount == 0)
+            //falling = true;
+            //print("true");
+            //print(number);
+            number++;
+        }
+        else
+        {
+            //falling = false;
+            //print("false");
+        }
+        if (jumpCount > 0)
+        {
+            falling = true;
+        }
+        else
+        {
+            falling = false;
+        }
+        bool OnPlatform = Platform.getOnPlatform();
+        if (Input.GetKey(KeyCode.W) && (OnPlatform == true && falling == false || jumpCount > 0 && falling == true && OnPlatform == false))
+        {
+            if (jumpCount == 0)
             {
                 jumpCount = 150;
             }
             animator.SetInteger("AnimState", 2);
+            //temp = y;
             y += 0.03f;
             if (Input.GetKey(KeyCode.A))
             {
@@ -49,6 +69,7 @@ public class AnimationController : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, target, 1);
             //GameObject.Find("Main Camera").transform.position = new Vector3(0 + x, 0 + y, -10);
             jumpCount--;
+            //falling = true;
         }
 
         else if (Input.GetKey(KeyCode.D))
@@ -58,18 +79,14 @@ public class AnimationController : MonoBehaviour
             x += 0.003f;
             Vector2 target = new Vector2(0.0f + x, 0.0f + y);
             transform.position = Vector2.MoveTowards(transform.position, target, 1);
-            //GameObject.Find("Main Camera").transform.position = new Vector3(0 + x, 0 + y, -10);
-            //transform.position = myPlay.position + myPos;
         }
 
         else if (Input.GetKey(KeyCode.A))
         {
             animator.SetInteger("AnimState", 3);
-            //transform.localRotation = Quaternion.Euler(0, 180, 0);
             x -= 0.003f;
             Vector2 target = new Vector2(0.0f + x, 0.0f + y);
             transform.position = Vector2.MoveTowards(transform.position, target, 1);
-            //GameObject.Find("Main Camera").transform.position = new Vector3(0 + x, 0 + y, -10);
         }
 
 
@@ -77,18 +94,28 @@ public class AnimationController : MonoBehaviour
         {
             animator.SetInteger("AnimState", 0);
         }
-        if (OnPlatform == false && jumpCount == 0)
+        //if (OnPlatform == false && jumpCount == 0 && transform.position.y != y)
+        //if (jumpCount == 0 && transform.position.y != y)
+        /*
+        if (jumpCount != 0)
         {
+            falling = true;
             if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
             {
                 y = transform.position.y - 0.007f;
             }
             else
             {
+                animator.SetInteger("AnimState", 2);
                 y = transform.position.y - 0.003f;
             }
             Vector2 target = new Vector2(0.0f + x, 0.0f + y);
-            //GameObject.Find("Main Camera").transform.position = new Vector3(0 + x, 0 + y, -10);
         }
+        else
+        {
+            falling = false;
+        }
+        */
+        //temp = y;
     }
 }
