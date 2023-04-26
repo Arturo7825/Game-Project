5016c06795e36933a62b6b89546974aea0e8ecb3
OmairHassan <ohassan@stu.naperville203.org>
2023-04-26 12:01:22 -0500

    Started slicing the enemy sprites

--- Game Design Group Project/Assets/TubeScript.cs
+++ Game Design Group Project/Assets/TubeScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TubeScript : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class TubeScript : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        levelManager.LoadLevel("Win");
+        SceneManager.LoadScene("Win");
 
     }
 }
