db124c4a1825f136af338650a5874756bf1bc911
Omair Hassan <ohassan@stu.naperville203.org>
2023-05-04 23:02:16 -0500

    Added some animations for boss

--- Game Design Group Project/Assets/Boss.cs
+++ Game Design Group Project/Assets/Boss.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    static float x = 0.0f;
+    static float y = 0.0f;
+    static float playerPosX = AnimationController.getX();
+    static float playerPosY = AnimationController.getY();
+    Animator animator;
+    float count = 0;
+    bool first = true;
+    Vector2 position;
+    public AudioSource Die;
+    public Rigidbody2D rigidbody;
+    public AudioSource Swing;
+    bool standing = false;
+    public int levelNumber;
+    //public float maxDifY;
+    //public float initialPosY;
+    //static float y = initialPosY;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (first == true)
+        {
+            animator = GetComponent<Animator>();
+            first = false;
+        }
+        x = transform.position.x;
+        y = transform.position.y;
+        playerPosX = AnimationController.getX();
+        playerPosY = AnimationController.getY();
+        //float playerPosY = AnimationController.getY()
+        if (x < playerPosX)
+        {
+            animator.SetInteger("AnimState", 1);
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            count = 0;
+            x += 0.02f;
+            standing = false;
+        }
+        else if (x > playerPosX)
+        {
+            animator.SetInteger("AnimState", 1);
+            transform.localRotation = Quaternion.Euler(0, 360, 0);
+            count = 0;
+            //print("b");
+            x -= 0.02f;
+            standing = false;
+        }
+        Vector2 target = new Vector2(x, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, target, 1);
     }
 }
